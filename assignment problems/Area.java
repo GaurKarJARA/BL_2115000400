89861e795ee4d3f9b50d83f7a4b8f760a222d564
Gaurav chaturvedi <94313244+GaurKarJARA@users.noreply.github.com>
2025-01-22 22:43:22 +0530

    Delete assignment problems directory

--- assignment problems/Area.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Area {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-       double radius = sc.nextDouble();
-        System.out.println("the area of circle is: "+(Math.PI*Math.pow(radius,2)));
-    }
-}
--- assignment problems/conversion.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class conversion {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int Celsius = sc.nextInt();
-        System.out.println("the temperature in fahrenheit is: "+((Celsius * 9/5) + 32));
-    }
-}
