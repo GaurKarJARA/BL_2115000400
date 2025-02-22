0a9873098bfa79851b0e80221597d3c8b02b084a
unknown <chaturgaurav35@gmail.com>
2025-02-22 22:34:07 +0530

    [gaurav chaturvedi],for - 05 Submission of Regex

---
+++ Regex/Advanced Problems/ExtractCurrencyValues.java
@@ -0,0 +1,13 @@
+import java.util.regex.*;
+public class ExtractCurrencyValues {
+    public static void main(String[] args) {
+        String text = "The price is $45.99, and the discount is 10.50.";
+        Pattern pattern = Pattern.compile("\\$?\\d+(\\.\\d{2})?");
+        Matcher matcher = pattern.matcher(text);
+
+        System.out.println("Extracted Currency: ");
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
