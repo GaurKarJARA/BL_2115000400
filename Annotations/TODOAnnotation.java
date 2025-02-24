e0508f079c20ec88b94aa52547cbc7de215bc889
unknown <chaturgaurav35@gmail.com>
2025-02-24 22:24:05 +0530

    [gaurav chaturvedi],for - 06 Submission of Annotations

---
+++ Annotations/TODOAnnotation.java
@@ -0,0 +1,28 @@
+import java.lang.annotation.*;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Todo {
+    String task();
+    String assignedTo();
+    String priority() default "MEDIUM";
+}
+
+class Project {
+    @Todo(task = "Implement login", assignedTo = "gaurav", priority = "HIGH")
+    public void login() {}
+
+    @Todo(task = "Fix UI bugs", assignedTo = "whoever", priority = "LOW")
+    public void fixUI() {}
+}
+public class TODOAnnotation {
+    public static void main(String[] args) throws Exception {
+        for (Method method : Project.class.getMethods()) {
+            if (method.isAnnotationPresent(Todo.class)) {
+                Todo todo = method.getAnnotation(Todo.class);
+                System.out.println(method.getName() + " - " + todo.task() + " (Assigned to: " + todo.assignedTo() + ", Priority: " + todo.priority() + ")");
+            }
+        }
+    }
+}
---
+++ Annotations/UseOverideCorrectly.java
@@ -0,0 +1,18 @@
+class Animal {
+    void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Bark!");
+    }
+}
+public class UseOverideCorrectly {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound();
+    }
+}
